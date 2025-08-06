2cfe610f665926b416be42b3ebef864f3bda155d
Younouss25 <ydiaw7@gmail.com>
2025-08-06 02:10:22 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,65 @@
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import {
+    getFirestore,
+    collection,
+    addDoc,
+    serverTimestamp,
+    onSnapshot,
+    query,
+    orderBy,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDs9mapr0u-IGr9pkvq8YYq9SmhHJYT1uE",
+    authDomain: "pixochat-5b8b9.firebaseapp.com",
+    projectId: "pixochat-5b8b9",
+    storageBucket: "pixochat-5b8b9.appspot.com",
+    messagingSenderId: "974545993617",
+    appId: "1:974545993617:web:346c5542bd197eee582430",
+    measurementId: "G-Q731X4GJKG"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+async function loginWithGoogle() {
+    try {
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+
+        const { user } = await signInWithPopup(auth, provider);
+
+        return { uid: user.uid, displayName: user.displayName };
+    } catch (error) {
+        if (error.code !== 'auth/cancelled-popup-request') {
+            console.error(error);
+        }
+        return null;
+    }
+}
+
+async function sendMessage(roomId, user, text) {
+    try {
+        await addDoc(collection(db, 'chat-rooms', roomId, 'messages'), {
+            uid: user.uid,
+            displayName: user.displayName,
+            text: text.trim(),
+            timestamp: serverTimestamp(),
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function getMessages(roomId, callback) {
+    return onSnapshot(
+        query(
+            collection(db, 'chat-rooms', roomId, 'messages'),
+            orderBy('timestamp', 'asc')
+        ),
+        (querySnapshot) => {
+            const messages = querySnapshot.docs.map((x) => ({
+                id: x.id,
+                ...x.data(),
+            }));
